4390b39292ebadff62f7ba90e5e3bffa96047ca4
John Prutton (PRTJOH016) <johnscon9@gmail.com>
2023-08-03 01:53:34 +0200

    add tpvcamera, still bug with the tank cam rotations

--- assets/tank/controller.gd
+++ assets/tank/controller.gd
@@ -3,11 +3,33 @@ extends CharacterBody3D
 
 const SPEED = 250
 const JUMP_VELOCITY = 5
+const SENSITIVITY: float = 0.001
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = 9.8
 
+var is_fpv = false
 
+
+@onready var fpv_camera: Camera3D = $Body/Turret/Barrel/Camera3D
+@onready var turret: Node3D = $Body/Turret
+@onready var barrel: Node3D = $Body/Turret/Barrel
+
+func _input(event):
+	if Input.is_action_just_pressed("enter_fpv"):
+		change_view()
+			
+	if (is_fpv or true) and event is InputEventMouseMotion:
+		var delta = event.relative
+		turret.rotate_y(SENSITIVITY * -delta.x)
+		barrel.rotate_x(SENSITIVITY * delta.y)
+		barrel.rotation_degrees.x = clampf(barrel.rotation_degrees.x, -25, 0)
+		
+
+func change_view():
+	is_fpv = not is_fpv
+	fpv_camera.current = is_fpv
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED if is_fpv else Input.MOUSE_MODE_VISIBLE
+		
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
@@ -21,7 +43,7 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	
 	
-	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	var input_dir = Input.get_vector("move_right", "move_left", "move_forward", "move_back")
 	
 	# Hanlde rotate
 	if input_dir.x != 0: rotate_y(delta * sign(input_dir.x) * deg_to_rad(30))
